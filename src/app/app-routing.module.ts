d9260a50281d2fb338abf330ada082e10d215eca
Pharadorn Boonruam <pharadorn.b@g-able.com>
2023-04-25 11:30:59 +0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,30 +4,6 @@ import { AdminGuard } from './core/guard/admin/admin.guard';
 import { UserGuardGuard } from './core/guard/user-guard.guard';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
-  },
-  {
-    path: 'module01',
-    loadChildren: () => import('./modules/module01/module01.module').then(m => m.Module01Module)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./modules/form/form.module').then(m => m.FormModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./modules/mat-table/mat-table.module').then(m => m.MatTableModule)
-  },
-  {
-    path: 'condition',
-    loadChildren: () => import('./modules/condition/condition.module').then(m => m.ConditionModule)
-  },
-  {
-    path: 'power-angular',
-    loadChildren: () => import('./modules/power-angular/power-angular.module').then(m => m.PowerAngularModule)
-  }
 ];
 
 @NgModule({
